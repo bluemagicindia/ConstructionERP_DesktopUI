cdf475e294f03866274422da7122ad2d83bc7167
Basharat Hussain <basharat.hussaini110@gmail.com>
2020-08-22 15:01:42 +0530

    Stagged
    Stagged Changes

---
+++ ConstructionERP_DesktopUI/Models/SupplierPaymentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionERP_DesktopUI.Models
+{
+    public class SupplierPaymentModel
+    {
+        public long ID { get; set; }
+
+        public long? SupplierBillID { get; set; }
+
+        public long Amount { get; set; }
+
+        public string Remarks { get; set; }
+
+        public DateTime PaidOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public bool Status { get; set; }
+
+        public SupplierBillModel SupplierBill { get; set; }
+
+
+    }
+}
