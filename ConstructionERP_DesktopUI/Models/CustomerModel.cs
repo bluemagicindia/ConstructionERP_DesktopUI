036b149b32f95d00d42bc85b565f7d389e231672
Basharat Hussain Bhat <basharat.hussaini110@gmail.com>
2020-10-01 12:23:21 +0530

    Add Customer Implemented
    Add Customer Implemented without flat addition

---
+++ ConstructionERP_DesktopUI/Models/CustomerModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ConstructionERP_DesktopUI.Models
+{
+    public class CustomerModel
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhonePrimary { get; set; }
+
+        public string PhoneSecondary { get; set; }
+
+        public string CurrentAddress { get; set; }
+
+        public string PAN { get; set; }
+
+        public string Aadhaar { get; set; }
+
+        public string ReferredBy { get; set; }
+
+        public IEnumerable<FlatModel> Flats { get; set; }
+    }
+}
---
+++ ConstructionERP_DesktopUI/Models/FlatModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ConstructionERP_DesktopUI.Models
+{
+    public class FlatModel
+    {
+        public long Id { get; set; }
+
+        public string Number { get; set; }
+
+        public long? ProjectID { get; set; }
+
+        public decimal EstimatedAmount { get; set; }
+
+        public decimal AverageTotal { get; set; }
+
+        public decimal EMI { get; set; }
+
+        public int Days { get; set; }
+
+        public ProjectModel Project { get; set; }
+    }
+}
