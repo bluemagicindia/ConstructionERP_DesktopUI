ccbeaf0f168003bc7070ef7cf1d9738971ff5b4c
Basharat Hussain Bhat <basharathosein@hotmail.com>
2020-10-15 09:29:07 +0530

    SupplierBillPayment Model Add to Source Control

---
+++ ConstructionERP_DesktopUI/Models/SupplierBillPaymentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionERP_DesktopUI.Models
+{
+    public class SupplierBillPaymentModel
+    {
+        public long ID { get; set; }
+        public long PaymentID { get; set; }
+        public DateTime Date { get; set; }
+        public string BillNo { get; set; }
+        public long BillAmount { get; set; }
+        public long PaymentAmount { get; set; }
+        public bool IsBill { get; set; }
+        public long Pending { get; set; }
+    }
+}
